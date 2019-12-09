0fb4a7640f3d368cc843f4de198c8f9a5ece3951
Arthur Lee <ruhtraeel@gmail.com>
2019-12-08 22:13:39 -0800

    Chapter 28 Lesson 11: Optimizing the Custom Store Hook

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -7,7 +7,7 @@ let actions = {};
 
 
 // This useStore can now be used to get the current state ([0]) or dispatch an action ([1])
-export const useStore = () => {
+export const useStore = (shouldListen = true) => {
     const setState = useState(globalState)[1];
 
     const dispatch = (actionIdentifier, payload) => {
@@ -21,15 +21,21 @@ export const useStore = () => {
 
     // Gets called whenever a component that uses this hook updates
     useEffect(() => {
-        // Every component using this custom hook will have its setState function added to the listeners array
-        // That way, every component that uses this custom hook can be rerendered when the state changes here
-        listeners.push(setState);
+
+        if (shouldListen) {
+            // Every component using this custom hook will have its setState function added to the listeners array
+            // That way, every component that uses this custom hook can be rerendered when the state changes here
+            listeners.push(setState);
+        }
+
 
         // Remove the listener when the component unmounts
         return () => {
-            listeners = listeners.filter(li => li !== setState)
+            if (shouldListen) {
+                listeners = listeners.filter(li => li !== setState)
+            }
         };
-    }, [setState]);
+    }, [setState, shouldListen]);
 
     return [globalState, dispatch]
 };
